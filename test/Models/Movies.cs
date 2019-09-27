5a85c1d0fee11296cd26d85a8a1061973480e9a6
likemyself <545194138@qq.com>
2019-09-27 17:02:56 +0800

    添加项目文件。

---
+++ test/Models/Movies.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace test.Models
+{
+    public partial class Movies
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public string ImgUrl { get; set; }
+        public string Country { get; set; }
+        public string Year { get; set; }
+        public string Tag { get; set; }
+        public string Actor { get; set; }
+        public string Cont { get; set; }
+    }
+}
---
+++ test/Models/MoviesContext.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace test.Models
+{
+    public partial class MoviesContext : DbContext
+    {
+        public MoviesContext()
+        {
+        }
+
+        public MoviesContext(DbContextOptions<MoviesContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Movies> Movies { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=.;user=sa;password=123@qwe;Database=Movies;ConnectRetryCount=0");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Movies>(entity =>
+            {
+                entity.Property(e => e.Actor).HasMaxLength(255);
+
+                entity.Property(e => e.Country).HasMaxLength(255);
+
+                entity.Property(e => e.ImgUrl).HasMaxLength(255);
+
+                entity.Property(e => e.Name).HasMaxLength(255);
+
+                entity.Property(e => e.Tag).HasMaxLength(255);
+
+                entity.Property(e => e.Url).HasMaxLength(255);
+
+                entity.Property(e => e.Year).HasMaxLength(255);
+            });
+        }
+    }
+}
